814b8147e190e220f3ca02217e8903295a3aac49
Skeletrox <android.svr@gmail.com>
2020-10-15 00:04:21 -0700

    enable user service

--- web_api/user_service/models.py
+++ web_api/user_service/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_name = models.CharField(max_length=64)
--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'user_service.apps.UserServiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
